3af4adc251946b6c726f7f3484d07d3bcfc4a8d8
Greg Wanner <greg@tegogroup.com>
2024-03-01 13:21:21 -0500

    Configured Student class properties and methods thru Step 5

---
+++ Module2Review/Student.cs
@@ -0,0 +1,30 @@
+﻿using Module2Review;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2Review
+{
+    public class Student
+    {
+        public Student()
+        {
+            
+        }
+        public string Name { get; set; }
+        public int Id { get; set; }
+        List<double> Grades { get; set; } = new List<double>();
+
+        public void AddGrade(double grade)
+        {
+            Grades.Add(grade);
+        };
+
+        public void AddGrade(params double[] grades)
+        {
+            Grades.AddRange(grades);
+        }
+    }
+}
